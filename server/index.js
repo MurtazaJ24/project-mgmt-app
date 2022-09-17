546f2f9630b1174adaa42f6ad71b4437817722f2
Murtaza J <murtazamj2003@gmail.com>
2022-09-17 18:05:48 +0530

    Initial

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
